6041befa5908bac8827f189600c99abeeb38acaa
Mori Adrian Rosland <mori@live.no>
2024-05-03 22:56:01 +0200

    feat: main function to gather inputs

--- index.ts
+++ index.ts
@@ -1,23 +1,71 @@
 import { ProjectData } from "./types";
-import readline from "readline";
+import * as readline from "readline";
 
-var data: ProjectData;
+var data: ProjectData = {
+  title: "",
+  description: "",
+  modal_title: "",
+  modal_text: "",
+  card_image: "",
+};
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question("Is this example useful? [y/n] ", (answer) => {
-  switch (answer.toLowerCase()) {
-    case "y":
-      console.log("Super!");
-      break;
-    case "n":
-      console.log("Sorry! :(");
-      break;
-    default:
-      console.log("Invalid answer!");
+function question(query: string): Promise<string> {
+  return new Promise((resolve) => {
+    rl.question(query, (answer) => {
+      resolve(answer);
+    });
+  });
+}
+
+async function main() {
+  try {
+    data.title = await question("Set project title [title field]: ");
+    console.log("title: ", data.title);
+
+    data.description = await question(
+      "Give project a description [description]: "
+    );
+    console.log("title: ", data.description);
+
+    data.modal_title = await question(
+      "Give the project modal a title [modal_title]: "
+    );
+    console.log("modal_title: ", data.modal_title);
+
+    data.modal_text = await question("Give the modal some text [modal_text]: ");
+    console.log("title: ", data.modal_text);
+
+    data.card_image = await question(
+      "Give the project a firebase image link [card_image]: "
+    );
+    console.log("title: ", data.card_image);
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+    var jsonData = JSON.stringify(data, null, 2);
+    console.log("Created the following json object:");
+    console.log(jsonData);
+    rl.close();
   }
-  rl.close();
-});
+}
+
+main();
+
+// function confirm_input(field: string) {
+//   rl.question("Confirm input? [y/n] ", (answer) => {
+//     switch (answer.toLowerCase()) {
+//       case "y":
+//         break;
+
+//       case "n":
+
+//       default:
+//         break;
+//     }
+//   });
+// }
